e1d5c48aa8c7cee26a0e15c6edf248b09e9c7dbb
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-03-23 19:43:42 +0600

    Pig game logic update 2

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,14 +1,19 @@
 'use strict';
+
 const scoreElement1 = document.getElementById('score--0');
 const scoreElement2 = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn--roll');
-const currentElement = document.querySelector('.current-score');
+const currentElement1 = document.querySelector('#current--0');
+const currentElement2 = document.querySelector('#current--1');
 
 scoreElement1.textContent = 0;
 scoreElement2.textContent = 0;
 diceElement.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let playerNumber = 0;
 
 rollBtn.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -17,7 +22,11 @@ rollBtn.addEventListener('click', function () {
 
   if (dice != 1) {
     currentScore += dice;
-    currentElement.textContent = currentScore;
-    console.log(currentScore);
+    document.getElementById(`current--${playerNumber}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${playerNumber}`).textContent = 0;
+    currentScore = 0;
+    playerNumber = playerNumber === 0 ? 1 : 0;
   }
 });
